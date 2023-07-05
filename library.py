7e533ce1c5c6335887dda838cbd8cf1ac74664d0
Zara Atasheneh <97232164+ZahraAtasheneh@users.noreply.github.com>
2023-07-05 09:59:55 +0300

    library.py

--- library.py
+++ library.py
@@ -21,3 +21,13 @@ class Library:
             ):
                 results.append(book)
         return results
+#This file contains the code for creating a library system.
+#defines two classes. Book class represents a book and has three attributes
+# An  __init__  method: gets called when a new Book object is created. The __init__ method takes in three parameters (title, author, genre)
+#and assigns them to the corresponding attributes of the object.
+#The library class represents a collection of books and has three methods: __init__ , add_book and search_books 
+#__init__ method in library: initializes an empty list to store the books when a new Library object is created.
+#add_book method: It takes a book parameter and adds the book to the list of books in the library. This method is used to add books to the library.
+#search_books method: It takes a search_criteria parameter, which is a term used to search for books in the library. 
+#The method goes through each book in the library and checks if the search criteria matches the book's title, author, or genre.
+#If there's a match, the book is added to the results list. Finally, the method returns the list of matching books.
